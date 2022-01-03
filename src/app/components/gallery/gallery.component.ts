56d17e7da1576674f8acb86aee2a58327b2029d2
sgmagic89 <sumanguha1989@gmail.com>
2022-01-03 14:23:58 +0530

    css tweaks

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 
 @Component({
@@ -10,9 +10,15 @@ export class GalleryComponent implements OnInit {
  images: any[] = [];
  background: any;
  index = 0;
+ screenHeight = 0;
+ @HostListener('window:resize', ['$event'])
+onResize(even?: any) {
+   this.screenHeight = window.innerHeight + 85;
+}
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.screenHeight = window.innerHeight + 85;
     this.dataService.getCategories().forEach(category => {
       if(category === 'Category A')
       this.dataService.getPaths(category).forEach(path => {

5fb2712a3db2dc75fa02ef49c4e146254622651b
sgmagic89 <sumanguha1989@gmail.com>
2022-01-17 12:03:17 +0530

    load images before showing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { slideInOutAnimation } from 'src/app/animations/animations';
 import { DataService } from 'src/app/services/data.service';
 
@@ -12,30 +13,36 @@ import { DataService } from 'src/app/services/data.service';
 export class HomeComponent implements OnInit {
   images: any[] = [];
   screenHeight = 0;
-  show = false;
+  imagesLoaded = 0;
+  imagesLoaded$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   @ViewChild('headerCarousel', {static: false}) headerCarousel: any;
   @HostListener('window:resize', ['$event'])
- onResize(even?: any) {
-    this.screenHeight = window.innerHeight-15;
- }
-   constructor(private dataService: DataService) { }
+  onResize(even?: any) {
+      this.screenHeight = window.innerHeight-15;
+  }
+   constructor(private dataService: DataService) {
+  }
  
    ngOnInit() {
-     this.show = false;
-     setTimeout(() => {
-       this.show = true
-      this.screenHeight = window.innerHeight-15;
-      this.dataService.getHomeImages().forEach(path => {
-        this.images.push(path);
-      });
-      setTimeout(() => {
-      this.headerCarousel.next();
-      });
-      setInterval(() => {
-        this.headerCarousel.next();
-      },5000)
-     },1000)
-
+    this.dataService.getHomeImages().forEach(path => {
+      this.images.push(path);
+    });
+    this.screenHeight = window.innerHeight-15;
+    this.imagesLoaded$.subscribe( loaded => {
+        if(loaded) {
+          this.screenHeight = window.innerHeight-15;
+          this.headerCarousel.next();
+          setInterval(() => {
+            this.headerCarousel.next();
+          },5000);
+        }
+    });
    }
- 
+
+   onAssetsLoad() {
+    this.imagesLoaded++;
+    if(this.imagesLoaded === this.images.length) {
+      this.imagesLoaded$.next(true);
+    }
+   }
 }

35f03c9908310f9f78e9c8c443c14ee4cd81f445
sgmagic89 <sumanguha1989@gmail.com>
2022-02-01 14:18:41 +0530

    preloader added, text justify

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { slideAnimation } from 'src/app/animations/animations';
 import { DataService } from 'src/app/services/data.service';
+import { LoaderService } from 'src/app/services/loader.service';
+import { PreLoaderService } from 'src/app/services/pre-loader.service';
 
 @Component({
   selector: 'app-about',
@@ -12,12 +15,26 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class AboutComponent implements OnInit {
   animationStart = false;
-  constructor(public dataService: DataService) { }
+  preLoadImages: any[] = [];
+  subscription: Subscription = <Subscription>{};
+  constructor(public dataService: DataService, 
+    private loader: LoaderService,
+    private preLoader: PreLoaderService) { }
 
   ngOnInit() {
-    setTimeout(() => {
-      this.animationStart = true;
-    },100)
+    this.loader.show();
+    this.preLoadImages.length = 0;
+    this.preLoadImages.push({path: 'assets/images/about/li.png'});
+    this.preLoadImages.push({path: 'assets/images/about/fb.png'});
+    this.preLoadImages.push({path: 'assets/images/about/as.png'});
+    this.preLoadImages.push({path: 'assets/images/about/arnab.png'});
+    this.subscription = this.preLoader.imagesLoaded$.subscribe(loaded => {
+      if(loaded) {
+        this.loader.hide();
+        setTimeout(() => {
+          this.animationStart = true;
+        },100);
+      }});
   }
 
 }
